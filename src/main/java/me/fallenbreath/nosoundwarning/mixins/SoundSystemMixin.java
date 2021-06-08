54eef8ceeb2ac4d0f4af43f4a1d453000b10f0c1
Fallen_Breath <yqx791125833@gmail.com>
2021-06-09 00:59:53 +0800

    comment for the priority

--- src/main/java/me/fallenbreath/nosoundwarning/mixins/SoundSystemMixin.java
+++ src/main/java/me/fallenbreath/nosoundwarning/mixins/SoundSystemMixin.java
@@ -6,6 +6,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+/**
+ * fabric carpet has a mixin priority of 42069, so here comes a higher one
+ */
 @Mixin(value = SoundSystem.class, priority = 123456)
 public abstract class SoundSystemMixin
 {
--- src/main/java/me/fallenbreath/nosoundwarning/mixins/SourceSetImplMixin.java
+++ src/main/java/me/fallenbreath/nosoundwarning/mixins/SourceSetImplMixin.java
@@ -5,6 +5,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+/**
+ * fabric carpet has a mixin priority of 42069, so here comes a higher one
+ */
 @Mixin(targets = "net.minecraft.client.sound.SoundEngine$SourceSetImpl", priority = 123456)
 public class SourceSetImplMixin
 {
